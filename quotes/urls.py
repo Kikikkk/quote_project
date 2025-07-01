24f8ce0e1300d5fd41acc318aa9155fc2f8a3e52
Angelina Kolosovskaya <95239836+Kikikkk@users.noreply.github.com>
2025-07-01 21:20:57 +0300

    Added the ability to like/dislike a quote

--- quotes/urls.py
+++ quotes/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_quote, name='add_quote'),
+    path('like/<int:quote_id>/', views.like_quote, name='like_quote'),
+    path('dislike/<int:quote_id>/', views.dislike_quote, name='dislike_quote'),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse, Http404
+
+from quotes.models import Quote
 from .forms import QuoteForm
 from .utils import get_random_quote
 
@@ -18,3 +21,25 @@ def home(request):
         quote.views += 1
         quote.save()
     return render(request, 'quotes/home.html', {'quote': quote})
+
+def like_quote(request, quote_id):
+    if request.method == 'POST':
+        try:
+            quote = Quote.objects.get(id=quote_id)
+            quote.likes += 1
+            quote.save()
+            return JsonResponse({'likes': quote.likes})
+        except Quote.DoesNotExist:
+            raise Http404
+    return JsonResponse({'error': 'Invalid method'}, status=405)
+
+def dislike_quote(request, quote_id):
+    if request.method == 'POST':
+        try:
+            quote = Quote.objects.get(id=quote_id)
+            quote.dislikes += 1
+            quote.save()
+            return JsonResponse({'dislikes': quote.dislikes})
+        except Quote.DoesNotExist:
+            raise Http404
+    return JsonResponse({'error': 'Invalid method'}, status=405)
