1b9d97b8dba6fc215eeb62c57fbbe1a7b06badaf
Angelina Kolosovskaya <95239836+Kikikkk@users.noreply.github.com>
2025-07-01 21:14:07 +0300

    Added home-page to display a random quote

--- quotes/views.py
+++ quotes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import QuoteForm
+from .utils import get_random_quote
 
 def add_quote(request):
     if request.method == 'POST':
@@ -10,3 +11,10 @@ def add_quote(request):
     else:
         form = QuoteForm()
     return render(request, 'quotes/add_quote.html', {'form': form})
+
+def home(request):
+    quote = get_random_quote()
+    if quote:
+        quote.views += 1
+        quote.save()
+    return render(request, 'quotes/home.html', {'quote': quote})
