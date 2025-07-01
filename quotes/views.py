3f0d2bf9df9e9a830c027ff55ca32c97b636e8d8
Angelina Kolosovskaya <95239836+Kikikkk@users.noreply.github.com>
2025-07-01 21:04:55 +0300

    Created form for adding a quote

--- quotes/views.py
+++ quotes/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import QuoteForm
 
-# Create your views here.
+def add_quote(request):
+    if request.method == 'POST':
+        form = QuoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home') 
+    else:
+        form = QuoteForm()
+    return render(request, 'quotes/add_quote.html', {'form': form})
